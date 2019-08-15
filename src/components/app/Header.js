51b603a541ec9aca1aa696b9684c896fd8297921
Joe Klause <joeklause@gmail.com>
2019-08-15 13:44:30 -0700

    updated image

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     renderHTML() {
         return /*HTML*/`
             <header>
-                <img src="http://pngimg.com/uploads/pokeball/pokeball_PNG30.png" alt="A Poke Ball">
+                <img src="../pokeball.png" alt="A Poke Ball">
                 <h1>The PokeDex</h1>
             </header>
         `;
