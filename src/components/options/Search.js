30731ef94d790af0620ef0e89e9ce775bb0f6ac7
Joe Klause <joeklause@gmail.com>
2019-08-15 14:28:15 -0700

    small html adjustments

--- src/components/options/Search.js
+++ src/components/options/Search.js
@@ -12,6 +12,7 @@ class Search extends Component {
                     <option value="defence">Minimum Defense Points</option>
                 </select>
                 <input id="user-search" name="user-search" input="text"></input>
+            </form>
         `;
     }
 }
