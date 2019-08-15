3260f05a554de9804ebb3a0fef987f8866d1cbbc
Joe Klause <joeklause@gmail.com>
2019-08-15 15:01:16 -0700

    radio button fixes

--- src/components/options/Sort.js
+++ src/components/options/Sort.js
@@ -9,19 +9,19 @@ class Sort extends Component {
                 <div>
                     <div>
                         <label for="sort-name">Pokemon Name</label>
-                        <input type="radio" id="sort-name" name="sort-name" checked=true>
+                        <input type="radio" id="sort-name" name="sort-option" checked=true>
                     </div>
                     <div>
                         <label for="sort-type">Pokemon Type</label>
-                        <input type="radio" id="sort-type" name="sort-type">
+                        <input type="radio" id="sort-type" name="sort-option">
                     </div>
                     <div>
                         <label for="sort-attack">Pokemon Attack</label>
-                        <input type="radio" id="sort-attack" name="sort-attack">
+                        <input type="radio" id="sort-attack" name="sort-option">
                     </div>
                     <div>
                         <label for="sort-defence">Pokemon Defence</label>
-                        <input type="radio" id="sort-defence" name="sort-defence">
+                        <input type="radio" id="sort-defence" name="sort-option">
                     </div>
                 </div>
             </div>
