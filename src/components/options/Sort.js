bb692cb1c659faf15e0aaf199e6ac585249d4a7d
Joe Klause <joeklause@gmail.com>
2019-08-16 16:59:37 -0700

    fixed sort items... more li css

--- src/components/options/Sort.js
+++ src/components/options/Sort.js
@@ -8,20 +8,20 @@ class Sort extends Component {
                 <h2>Sort The PokeDex</h2>
                 <div>
                     <div>
-                        <label for="sort-name">Pokemon Name</label>
                         <input type="radio" id="sort-name" name="sort-option" checked=true>
+                        <label for="sort-name">Pokemon Name</label>
                     </div>
                     <div>
-                        <label for="sort-type">Pokemon Type</label>
                         <input type="radio" id="sort-type" name="sort-option">
+                        <label for="sort-type">Pokemon Type</label>
                     </div>
                     <div>
-                        <label for="sort-attack">Pokemon Attack</label>
                         <input type="radio" id="sort-attack" name="sort-option">
+                        <label for="sort-attack">Pokemon Attack</label>
                     </div>
                     <div>
-                        <label for="sort-defence">Pokemon Defence</label>
                         <input type="radio" id="sort-defence" name="sort-option">
+                        <label for="sort-defence">Pokemon Defence</label>
                     </div>
                 </div>
             </div>
