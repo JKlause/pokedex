402519a49f4b065900f27c5c6fd867e5881fdd5f
Joe Klause <joeklause@gmail.com>
2019-08-16 11:42:19 -0700

    dynamic background color

--- src/components/pokedex/PokedexCard.js
+++ src/components/pokedex/PokedexCard.js
@@ -4,7 +4,7 @@ class PokedexCard extends Component {
     renderHTML() {
         const pokemon = this.props.pokemon;
         return /*html*/`
-            <li style="background-color:#F8D030">
+            <li style="background-color:${pokemon.color_1}>
                 <h3 id="li-name">${pokemon.pokemon.toUpperCase()}</h3>
                 <img id="li-image" src="${pokemon.url_image}" alt="${pokemon.pokemon.toUpperCase()}">
                 <div id="li-data">
